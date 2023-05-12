8101bcc12465dd52a19ba38a763192b4ee743922
Marina277 <123168025+Marina277@users.noreply.github.com>
2023-05-12 15:00:34 +0300

    Deleted 'flash' as unused import

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template, request, flash
+from flask import Flask, redirect, url_for, render_template, request
 from flask_mail import Mail, Message
 
 
